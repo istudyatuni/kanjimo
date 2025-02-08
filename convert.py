681830abf0cdce18064ab2ea27416cca46284591
Ilia <istudyatuni@gmail.com>
2025-02-08 16:00:23 +0300

    convert: check .cbor hash

--- convert.py
+++ convert.py
@@ -1,16 +1,22 @@
 from pathlib import Path
 
-import json
 import cbor2
+import hashlib
+import json
 import zipfile
 
-EXTRA = 'data/extra.json'
-DIR = 'data'
+DIR = Path('data')
 BASE_FILE = 'kanjiapi_full'
+
+CBOR_FILE = BASE_FILE + '.cbor'
 JSON_FILE = BASE_FILE + '.json'
-SOURCE = Path(DIR).joinpath(JSON_FILE)
-SOURCE_ZIP = Path(DIR).joinpath(f'{BASE_FILE}.zip')
-TARGET = Path(DIR).joinpath(f'{BASE_FILE}.cbor')
+
+SOURCE = DIR.joinpath(JSON_FILE)
+SOURCE_ZIP = DIR.joinpath(f'{BASE_FILE}.zip')
+EXTRA = DIR.joinpath('extra.json')
+TARGET = DIR.joinpath(CBOR_FILE)
+
+LOCK = DIR.joinpath('data.lock.json')
 
 type Kanji = dict[str, list[str]]
 type KanjiDict = dict[str, Kanji]
@@ -56,6 +62,44 @@ def check_files() -> bool:
     return False
 
 
+def save_cbor(data: bytes):
+    with open(TARGET, mode='wb') as file:
+        file.write(data)
+
+
+def check_cbor(data: bytes):
+    key = CBOR_FILE
+
+    if not Path(LOCK).exists():
+        with open(LOCK, mode='w') as file:
+            file.write('{}')
+
+    hasher = hashlib.sha256(data)
+    new_hash = hasher.hexdigest()
+
+    with open(LOCK) as file:
+        content = json.load(file)
+
+    if key in content and content[key] != '':
+        orig_hash = content[key]
+        if orig_hash != new_hash:
+            print(f'hash of {TARGET} changed. to update, remove hash from {LOCK}')
+            print(f'old {orig_hash}')
+            print(f'new {new_hash}')
+            return
+
+        if not TARGET.exists():
+            save_cbor(data)
+        else:
+            print(f'{TARGET} not changed')
+
+        return
+
+    save_cbor(data)
+    with open(LOCK, mode='w') as file:
+        json.dump({key: new_hash}, file)
+
+
 def main():
     if check_files():
         return
@@ -81,8 +125,8 @@ def main():
     extra = len(filtered) - total
     print(f'processed {total} kanji from kanjiapi and {extra} extra kanji')
 
-    with open(TARGET, 'wb') as file:
-        cbor2.dump(filtered, file)
+    encoded: bytes = cbor2.dumps(filtered)
+    check_cbor(encoded)
     # json size is about x2 of cbor
     # with open('data/kanjiapi_full-converted.json', 'w') as file:
     #     json.dump(filtered, file)
